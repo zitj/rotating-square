d0c9a113def2ca442c23731306ce0467bc30fbf2
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2021-08-19 12:14:22 +0200

    Start rotating

--- js/rotate.js
+++ js/rotate.js
@@ -1,13 +1,13 @@
-const waterrmelon = document.getElementById('waterrmelon');
+const watermelon = document.getElementById('watermelon');
+const x = document.getElementById('x');
+const y = document.getElementById('y');
 
+let mouseDown = false;
 
-
-
-for(let i = 0; i <= 7; i++){
-    
-    let template = ''; 
-    template = `
-    <div class="container">
+for (let i = 0; i <= 7; i++) {
+	let addSeeds = '';
+	addSeeds = `
+    <div class="containerOfSeeds">
         <div class="seed"></div>
         <div class="seed"></div>
         <div class="seed"></div>
@@ -17,5 +17,33 @@ for(let i = 0; i <= 7; i++){
     </div>
     `;
 
-    waterrmelon.innerHTML += template;
+	watermelon.innerHTML += addSeeds;
 }
+
+watermelon.addEventListener('mousedown', (e) => {
+	console.log(e);
+	mouseDown = true;
+	console.log(mouseDown);
+	if (mouseDown) {
+	}
+});
+
+watermelon.addEventListener('mousemove', (e) => {
+	x.innerText = `x = ${e.x}`;
+	y.innerText = `y = ${e.y}`;
+	if (e.y > 350 && e.x < 870) {
+		console.log(e.y - 350);
+		watermelon.style.transform = `rotate(-${e.y - 350}deg)`;
+	}
+	if (e.y > 350 && e.x > 870) {
+		console.log(e.y - 350);
+		watermelon.style.transform = `rotate(${e.y - 350}deg)`;
+	}
+});
+
+watermelon.addEventListener('mouseup', (e) => {
+	console.log(e);
+	mouseDown = false;
+	console.log(mouseDown);
+	watermelon.style.backgroundColor = '#ff4757';
+});
