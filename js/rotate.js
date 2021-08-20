12e5d04bfe635cee234d6db89edc9f30a44735f5
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2021-08-20 12:04:57 +0200

    Rotates just fine

--- js/rotate.js
+++ js/rotate.js
@@ -1,23 +1,13 @@
-const watermelon = document.getElementById('watermelon');
-const watermelonContainer = document.querySelector('.watermelonContainer');
 const x = document.getElementById('x');
 const y = document.getElementById('y');
 const rotation = document.getElementById('rotation');
-let rotationCounter = 1;
 
-let rotate;
-let mouseDown = false;
+let watermelon = document.querySelector('#watermelon');
+let watermelonBoundingRect = watermelon.getBoundingClientRect();
 
-const state = {
-	mouse: {
-		x: 0,
-		y: 0,
-	},
-	watermelon: {
-		x: 280,
-		y: 300,
-		rotation: 0,
-	},
+let watermelonCenter = {
+	x: watermelonBoundingRect.left + watermelonBoundingRect.width / 2,
+	y: watermelonBoundingRect.top + watermelonBoundingRect.height / 2,
 };
 
 for (let i = 0; i <= 7; i++) {
@@ -32,21 +22,13 @@ for (let i = 0; i <= 7; i++) {
         <div class="seed"></div>
     </div>
     `;
-
 	watermelon.innerHTML += addSeeds;
 }
 
-let watermelonCenter = {
-	x: watermelon.clientLeft + watermelon.clientWidth / 2,
-	y: watermelon.clientTop + watermelon.clientHeight / 2,
-};
-
-watermelonContainer.addEventListener('mousemove', (e) => {
-	// let angle = Math.atan2(e.pageX - watermelonCenter.x, -(e.pageY - watermelonCenter.y)) * (180 / Math.PI);
+document.addEventListener('mousemove', (e) => {
 	x.innerText = `x = ${e.x}`;
 	y.innerText = `y = ${e.y}`;
-	if (e.y > 330) {
-		watermelon.style.transform = `rotate(-${e.y - 360}deg)`;
-		rotation.innerText = `rotation = -${e.y - 360} deg`;
-	}
+	let angle = Math.atan2(e.pageX - watermelonCenter.x, -(e.pageY - watermelonCenter.y)) * (180 / Math.PI);
+	watermelon.style.transform = `rotate(${angle}deg)`;
+	rotation.innerText = `rotation = ${angle.toFixed(0)} deg`;
 });
